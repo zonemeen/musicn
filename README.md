12d5526dbac60a11d153541b4c1d0fd382de07ee
miqilin21 <994718917@qq.com>
2021-08-27 10:39:44 +0800

    feat: SQ sound quality of migu service

--- README.md
+++ README.md
@@ -18,21 +18,21 @@ $ npm install musicn -g
 
 ```bash
 # default use migu's service
-$ musicn 以父之名
+$ musicn 七里香
 # or use NetEase Cloud Music's service
-$ musicn -s 163 以父之名
+$ musicn -s 163 七里香
 # or use Kugou's service
-$ musicn -s kugou 以父之名
+$ musicn -s kugou 七里香
 ```
 
 For short:
 
 ```bash
-$ msc 以父之名
+$ msc 七里香
 # or
-$ msc -s 163 以父之名
+$ msc -s 163 七里香
 # or
-$ msc -s kugou 以父之名
+$ msc -s kugou 七里香
 ```
 
 ## Configuration
--- src/index.js
+++ src/index.js
@@ -23,25 +23,22 @@ const name = options.service === undefined ? "migu" : options.service;
   const page = await browser.newPage();
   await page.setViewport({ width: 1280, height: 720 });
 
-  let strUrl, musicUrl, downloadUrl;
+  let searchUrl, musicUrl, downloadUrl;
 
   if (name === "kugou") {
-    strUrl =
-      "http://mobilecdn.kugou.com/api/v3/search/song?format=json&keyword=" +
-      text;
-    await page.goto(strUrl);
+    searchUrl = `http://mobilecdn.kugou.com/api/v3/search/song?format=json&keyword=${text}`;
+    await page.goto(searchUrl);
     await page.waitForSelector("body");
 
     const jsonData = await page.evaluate(
       () => document.querySelector("body").innerText
     );
     const { hash, album_id } = JSON.parse(jsonData).data.info[0];
-    musicUrl =
-      "https://www.kugou.com/song/#hash=" + hash + "&album_id=" + album_id;
+    musicUrl = `https://www.kugou.com/song/#hash=${hash}&album_id=${album_id}`;
     await page.goto(musicUrl);
   } else if (name === "163") {
-    strUrl = "https://music.163.com/#/search/m/?s=" + text;
-    await page.goto(strUrl);
+    searchUrl = `https://music.163.com/#/search/m/?s=${text}`;
+    await page.goto(searchUrl);
     await page.waitForSelector(".g-iframe");
 
     const iframe = await page.frames().find((f) => f.name() === "contentFrame");
@@ -49,33 +46,33 @@ const name = options.service === undefined ? "migu" : options.service;
       () => document.querySelector(".text > a")?.href
     );
     if (!href) {
-      console.log("网易云音乐-查无此歌曲");
+      console.log("查无此歌曲");
       return await browser.close();
     }
     const id = href.split("=")[1];
-    musicUrl = "http://music.163.com/song/media/outer/url?id=" + id + ".mp3";
+    musicUrl = `http://music.163.com/song/media/outer/url?id=${id}.mp3`;
     await page.goto(musicUrl);
   } else {
-    strUrl =
-      "https://pd.musicapp.migu.cn/MIGUM3.0/v1.0/content/search_all.do?&text=" +
-      text +
-      "&pageSize=1&searchSwitch={song:1}";
-    await page.goto(strUrl);
+    searchUrl = `https://pd.musicapp.migu.cn/MIGUM3.0/v1.0/content/search_all.do?&text=${text}&pageSize=1&searchSwitch={song:1}`;
+    await page.goto(searchUrl);
     await page.waitForSelector("body");
     const jsonData = await page.evaluate(
       () => document.querySelector("body").innerText
     );
-    const hqSource = JSON.parse(
-      jsonData
-    ).songResultData?.result[0].newRateFormats.find(
-      (format) => format.formatType === "HQ"
-    ).url;
-    if (!hqSource) {
+    const newRateFormats = JSON.parse(jsonData).songResultData?.result[0]
+      .newRateFormats;
+    if (!newRateFormats) {
       console.log("查无此歌曲");
       return await browser.close();
     }
-    const { pathname } = new URL(hqSource);
-    downloadUrl = "https://freetyst.nf.migu.cn/" + pathname;
+    let hqSource;
+    for (let format of newRateFormats) {
+      if (format.formatType === "SQ" || format.formatType === "HQ") {
+        hqSource = format;
+      }
+    }
+    const { pathname } = new URL(hqSource.url || hqSource.androidUrl);
+    downloadUrl = `https://freetyst.nf.migu.cn/${pathname}`;
   }
   const $http = name === "163" ? http : https;
   if (!(name === "migu")) {
@@ -86,7 +83,7 @@ const name = options.service === undefined ? "migu" : options.service;
     );
   }
   if (!downloadUrl && name === "163") {
-    console.log("网易云音乐-vip歌曲无权限下载");
+    console.log("vip歌曲无权限下载");
     return await browser.close();
   }
   $http.get(downloadUrl, (res) => {
