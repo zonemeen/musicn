8a57a57d44cb0e5d9e95bb0a3d6402f193f46348
zonemeen <miqilin18@gmail.com>
2022-06-09 18:32:57 +0800

    chore: 增加eslint配置

--- bin/musicn.js
+++ bin/musicn.js
@@ -1,10 +1,4 @@
 #!/usr/bin/env node
-'use strict'
 import musicn from '../lib/index.js'
 
-// Provide a title to the process in `ps`
-process.title = 'musicn'
-
-!(async () => {
-  await musicn()
-})()
+musicn()
