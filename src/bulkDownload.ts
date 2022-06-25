57ee60ce3a20cc8dce1f745d100a9569f1dbba29
zonemeen <994718917@qq.com>
2022-06-25 22:33:12 +0800

    refactor: 使用Promise.all批量下载

--- src/bulkDownload.ts
+++ src/bulkDownload.ts
@@ -34,7 +34,6 @@ const multiBar = new cliProgress.MultiBar({
 })
 
 const download = (
-  index: number,
   {
     songName,
     songDownloadUrl,
@@ -42,86 +41,89 @@ const download = (
     songSize,
     options,
     serviceName,
-  }: SongInfo
+  }: SongInfo,
+  index: number
 ) => {
-  if (songNameMap.has(songName)) {
-    songNameMap.set(songName, songNameMap.get(songName) + 1)
-    const [name, extension] = songName.split('.')
-    const newName = `${name}(${songNameMap.get(songName)})`
-    songName = `${newName}.${extension}`
-  } else {
-    songNameMap.set(songName, 0)
-  }
-  const { lyric: withLyric = false, path: targetDir = process.cwd() } = options
-  const songPath = path.join(targetDir, songName)
-  const lrcName = `${songName.slice(0, songName.lastIndexOf('.'))}.lrc`
-  const lrcPath = path.join(targetDir, lrcName)
-  checkFileExist(songPath, songName)
-  barList.push(multiBar.create(songSize, 0, { file: songName }))
+  return new Promise((resolve, reject) => {
+    if (songNameMap.has(songName)) {
+      songNameMap.set(songName, songNameMap.get(songName) + 1)
+      const [name, extension] = songName.split('.')
+      const newName = `${name}(${songNameMap.get(songName)})`
+      songName = `${newName}.${extension}`
+    } else {
+      songNameMap.set(songName, 0)
+    }
+    const { lyric: withLyric = false, path: targetDir = process.cwd() } =
+      options
+    const songPath = path.join(targetDir, songName)
+    const lrcName = `${songName.slice(0, songName.lastIndexOf('.'))}.lrc`
+    const lrcPath = path.join(targetDir, lrcName)
+    checkFileExist(songPath, songName)
+    barList.push(multiBar.create(songSize, 0, { file: songName }))
 
-  if (!fs.existsSync(targetDir)) {
-    fs.mkdirSync(targetDir)
-  }
+    if (!fs.existsSync(targetDir)) {
+      fs.mkdirSync(targetDir)
+    }
 
-  const fileReadStream = got.stream(songDownloadUrl)
+    const fileReadStream = got.stream(songDownloadUrl)
 
-  const onError = (err: unknown) => {
-    delUnfinishedFiles(targetDir, Array.from(unfinishedPathMap.keys()))
-    console.error(red(`${songName}下载失败，报错信息：${err}`))
-    process.exit(1)
-  }
+    const onError = (err: unknown) => {
+      delUnfinishedFiles(targetDir, Array.from(unfinishedPathMap.keys()))
+      console.error(red(`${songName}下载失败，报错信息：${err}`))
+      reject(err)
+      process.exit(1)
+    }
 
-  fileReadStream.on('response', async () => {
-    // 是否下载歌词
-    if (withLyric && serviceName === 'netease') {
-      try {
-        const {
-          lrc: { lyric },
-        } = await got(lyricDownloadUrl).json()
-        if (lyric) {
-          const lrcFile = fs.createWriteStream(lrcPath)
-          lrcFile.write(lyric)
+    fileReadStream.on('response', async () => {
+      // 是否下载歌词
+      if (withLyric && serviceName === 'netease') {
+        try {
+          const {
+            lrc: { lyric },
+          } = await got(lyricDownloadUrl).json()
+          if (lyric) {
+            const lrcFile = fs.createWriteStream(lrcPath)
+            lrcFile.write(lyric)
+          }
+        } catch (err) {
+          onError(err)
         }
-      } catch (err) {
-        onError(err)
       }
-    }
-    if (withLyric && serviceName === 'migu') {
+      if (withLyric && serviceName === 'migu') {
+        try {
+          await promisifyPipeline(
+            got.stream(lyricDownloadUrl),
+            fs.createWriteStream(lrcPath)
+          )
+        } catch (err) {
+          onError(err)
+        }
+      }
+
+      // 防止`onError`被调用两次
+      fileReadStream.off('error', onError)
+
       try {
-        await promisifyPipeline(
-          got.stream(lyricDownloadUrl),
-          fs.createWriteStream(lrcPath)
-        )
+        unfinishedPathMap.set(songPath, true)
+        await promisifyPipeline(fileReadStream, fs.createWriteStream(songPath))
+        unfinishedPathMap.delete(songPath)
+        resolve()
       } catch (err) {
         onError(err)
       }
-    }
+    })
 
-    // 防止`onError`被调用两次
-    fileReadStream.off('error', onError)
+    fileReadStream.on('downloadProgress', ({ transferred }) => {
+      barList[index].update(transferred)
+    })
 
-    try {
-      unfinishedPathMap.set(songPath, true)
-      await promisifyPipeline(fileReadStream, fs.createWriteStream(songPath))
-      unfinishedPathMap.delete(songPath)
-    } catch (err) {
-      onError(err)
-    }
+    fileReadStream.once('error', onError)
   })
-
-  fileReadStream.on('downloadProgress', ({ transferred }) => {
-    barList[index].update(transferred)
-  })
-
-  fileReadStream.once('error', onError)
 }
 
-const bulkDownload = (songs: SongInfo[]) => {
+const bulkDownload = async (songs: SongInfo[]) => {
   const { path: targetDir = process.cwd() } = songs[0].options
   console.log(green('下载开始...'))
-  for (const [index, song] of songs.entries()) {
-    download(index, song)
-  }
   multiBar.on('stop', () => {
     if (!Array.from(unfinishedPathMap.keys()).length) {
       console.log(green('下载完成'))
@@ -129,5 +131,8 @@ const bulkDownload = (songs: SongInfo[]) => {
     delUnfinishedFiles(targetDir, Array.from(unfinishedPathMap.keys()))
     process.exit(0)
   })
+  await Promise.all(
+    songs.map(async (song, index) => await download(song, index))
+  )
 }
 export default bulkDownload
--- src/search.ts
+++ src/search.ts
@@ -10,6 +10,7 @@ const search = async ({ text, options, serviceName }: SongInfo) => {
     console.error(red(`请输入歌曲名称或歌手名字`))
     process.exit(1)
   }
+  // @ts-ignore
   const spinner = ora(cyan('搜索ing')).start()
   if (serviceName === 'netease') {
     const searchUrl = `https://music.163.com/api/search/get/web?s=${removePunctuation(
@@ -31,8 +32,8 @@ const search = async ({ text, options, serviceName }: SongInfo) => {
     const searchUrl = `https://pd.musicapp.migu.cn/MIGUM3.0/v1.0/content/search_all.do?text=${removePunctuation(
       text
     )}&searchSwitch={song:1}`
-    const searchResult: any = await got(searchUrl).json()
-    searchSongs = searchResult.songResultData?.result || []
+    const { songResultData } = await got(searchUrl).json()
+    searchSongs = songResultData?.result || []
   }
   if (!searchSongs.length) {
     spinner.fail(red(`没搜索到 ${text} 的相关结果`))
