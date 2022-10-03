658f1587cef024d6cf426d9f8b45d10facad4c22
zonemeen <994718917@qq.com>
2022-10-03 10:29:01 +0800

    chore: 命令行交互库换用cac

--- src/command.ts
+++ src/command.ts
@@ -1,45 +1,32 @@
-import { Command } from 'commander'
-import { cyan, red } from 'colorette'
+import { cac } from 'cac'
+import { red } from 'colorette'
 import updateNotifier from 'update-notifier'
 import pkg from '../package.json'
 
-const program = new Command()
-
 export default (() => {
+  const cli = cac('musicn')
   updateNotifier({ pkg }).notify()
-  program.name('musicn or msc').usage('<text>').version(pkg.version)
-  program.on('--help', () => {
-    console.log('')
-    console.log(cyan('普通下载(默认是咪咕的服务):'))
-    console.log(`${cyan('$ ')}msc 晴天`)
-    console.log(cyan('酷我服务下载:'))
-    console.log(`${cyan('$ ')}msc 晴天 -k`)
-    console.log(cyan('网易云服务下载:'))
-    console.log(`${cyan('$ ')}msc 晴天 -w`)
-    console.log(cyan('附带歌词:'))
-    console.log(`${cyan('$ ')}msc 晴天 -l`)
-    console.log(cyan('下载路径:'))
-    console.log(`${cyan('$ ')}msc 晴天 -p ./music`)
-    console.log('')
-  })
 
-  program
-    .option('-n, --number <number>', '搜索时的页码数', '1')
+  cli
+    .option('-n, --number <number>', '搜索时的页码数', { default: '1' })
     .option('-l, --lyric', '是否下载歌词')
     .option('-p, --path <path>', '音乐批量下载的目标目录路径')
     .option('-m, --migu', '默认是咪咕的服务')
     .option('-k, --kuwo', '酷我的服务')
     .option('-w, --wangyi', '网易云的服务')
 
-  program.parse(process.argv)
+  cli.help()
 
-  const options = program.opts()
-  const { kuwo, wangyi, migu } = options
+  cli.version(pkg.version)
+
+  const { args, options } = cli.parse()
+  const { kuwo, wangyi, migu, help, version } = options
   const serviceNum = [kuwo, wangyi, migu].filter(Boolean).length
   if (serviceNum > 1) {
     console.error(red('同时只允许输入一种服务'))
     process.exit(1)
   }
+  if (help || version) process.exit()
   options.migu = !(wangyi || kuwo)
-  return { text: program.args.join(' '), options }
+  return { text: args.join(' '), options }
 })()
