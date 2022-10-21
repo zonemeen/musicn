a0eecf8657f1d515a35e630b5bb8c74ace60de22
zonemeen <miqilin18@gmail.com>
2022-10-21 17:05:16 +0800

    chore: 命令行交互库重新用inquirer主库

--- src/choose.ts
+++ src/choose.ts
@@ -1,16 +1,17 @@
-import checkbox from '@inquirer/checkbox'
-import { cyan, bold } from 'colorette'
+import inquirer from 'inquirer'
 import names from './names'
-import type { SongInfo } from './types'
+import { SongInfo } from './types'
 
-const choose = async ({ searchSongs, options }: SongInfo) =>
-  await checkbox({
-    message: '选择歌曲',
-    instructions: ` (按下 ${bold(cyan('<空格键>'))} 单选, ${bold(cyan('<a键>'))} 全选, ${bold(
-      cyan('<i键>')
-    )} 反选, ${bold(cyan('<回车键>'))} 确认)`,
-    pageSize: 20,
-    choices: searchSongs.map((song, index) => names(song, index, options)),
-  })
+const choose = ({ searchSongs, options }: SongInfo) =>
+  inquirer.prompt([
+    {
+      type: 'checkbox',
+      name: 'songs',
+      message: '选择歌曲',
+      pageSize: 20,
+      dontShowHints: false,
+      choices: searchSongs.map((song, index) => names(song, index, options)),
+    },
+  ])
 
 export default choose
