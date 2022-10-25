11f79c21cd7e046cd480c1d2ac8d8d970c1ba043
zonemeen <miqilin18@gmail.com>
2022-10-25 11:06:52 +0800

    chore: 去除magic-string依赖重写esbuild插件

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,15 +1,14 @@
 import fs from 'fs'
 import { defineConfig } from 'tsup'
-import MagicString from 'magic-string'
 
 const removeSomeCodePlugin = {
   name: 'removeSomeCodePlugin',
   setup(build: any) {
     build.onLoad({ filter: /command.ts/ }, async (args: any) => {
       const sourceCode = await fs.promises.readFile(args.path, 'utf8')
-      const magicString = new MagicString(sourceCode)
-      magicString.update(944, 993, '')
-      return { contents: magicString.toString() }
+      return {
+        contents: sourceCode.split('process.stdout.write(JSON.stringify(content))').join(''),
+      }
     })
   },
 }
