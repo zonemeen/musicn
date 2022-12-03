925af686a1ff6f0e356536f3a1d95146e03f6fe2
zonemeen <miqilin18@gmail.com>
2022-12-03 15:22:31 +0800

    fix: kuwo因CA证书过期导致无法获取size

--- src/searchType/kuwo.ts
+++ src/searchType/kuwo.ts
@@ -10,27 +10,22 @@ const kuwoSearchSong = async (text: string, pageNum: string) => {
     searchUrl
   ).json()
   const totalSongCount = Number(TOTAL) || undefined
-  const detailResults = await Promise.all(
-    abslist.map(({ DC_TARGETID }) => {
-      const detailUrl = `https://www.kuwo.cn/api/v1/www/music/playUrl?mid=${DC_TARGETID}&type=1`
-      return got(detailUrl).json()
+  await Promise.all(
+    abslist.map(async (item) => {
+      const detailUrl = `https://www.kuwo.cn/api/v1/www/music/playUrl?mid=${item.DC_TARGETID}&type=1`
+      const {
+        data: { url },
+      }: any = await got(detailUrl).json()
+      const size = await getSongSizeByUrl(url)
+      Object.assign(item, {
+        url,
+        size,
+        name: item.NAME,
+        songName: `${item.ARTIST.replaceAll('&', ',')} - ${item.NAME}.mp3`,
+        lyricUrl: `https://m.kuwo.cn/newh5/singles/songinfoandlrc?musicId=${item.DC_TARGETID}`,
+      })
     })
   )
-  abslist.forEach((item, index) => {
-    const {
-      data: { url },
-    }: any = detailResults[index]
-    Object.assign(item, {
-      url,
-      name: item.NAME,
-      songName: `${item.ARTIST.replaceAll('&', ',')} - ${item.NAME}.mp3`,
-      lyricUrl: `https://m.kuwo.cn/newh5/singles/songinfoandlrc?musicId=${item.DC_TARGETID}`,
-    })
-  })
-  const sizeResults = await Promise.all(abslist.map(({ url }) => getSongSizeByUrl(url)))
-  abslist.forEach((item, index) => {
-    item.size = sizeResults[index] as number
-  })
   return {
     searchSongs: abslist,
     totalSongCount,
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,4 @@
-import got from 'got'
-import { red } from 'colorette'
+import https from 'https'
 import { inflate } from 'zlib'
 import type { Artist } from './types'
 
@@ -15,20 +14,24 @@ export const joinSingersName = (singers: Artist[]) => {
 export const getSongSizeByUrl = (url: string) => {
   if (!url) return Promise.reject(new Error('无效的Url'))
   return new Promise(async (resolve, reject) => {
-    try {
-      const request = got.stream(url)
-      request.on('response', async (res) => {
-        const length = parseInt(<string>res.headers['content-length'])
-        if (!isNaN(length) && res.statusCode === 200) {
-          resolve(length)
-        } else {
-          reject(new Error('无法获取文件大小'))
+    https
+      .get(
+        url,
+        {
+          rejectUnauthorized: false,
+        },
+        (res) => {
+          const length = parseInt(<string>res.headers['content-length'])
+          if (!isNaN(length) && res.statusCode === 200) {
+            resolve(length)
+          } else {
+            reject(new Error('无法获取文件大小'))
+          }
         }
+      )
+      .on('error', (e: any) => {
+        reject(e)
       })
-      request.once('error', async (e: any) => reject(e))
-    } catch (err) {
-      console.error(red(`${err}`))
-    }
   })
 }
 
