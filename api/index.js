00b67d58fbc1aaa3532131fe09b72c6f0256d97a
zonemeen <miqilin18@gmail.com>
2023-03-27 12:31:48 +0800

    fix: vercel线上服务器读取不到html模板

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 import { readFileSync } from 'fs'
 
 export default async (req, res) => {
-  res.send(readFileSync('template/music.html', 'utf-8'))
+  res.send(readFileSync('music.html', 'utf-8'))
 }
